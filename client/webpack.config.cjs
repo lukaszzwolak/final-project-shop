7a68afa58dffd63dd572260b8d422fc67f91ed10
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-09-20 16:17:12 +0200

    chore(client): webpack + dev-server konfiguracja

---
+++ client/webpack.config.cjs
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.[contenthash].js",
+    clean: true,
+    publicPath: "/",
+  },
+  mode: process.env.NODE_ENV || "development",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
+  resolve: { extensions: [".js"] },
+  devServer: {
+    port: 5173,
+    historyApiFallback: true,
+    hot: true,
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
+};
